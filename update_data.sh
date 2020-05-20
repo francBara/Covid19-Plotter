282c7857b26b27b9e61260b1294bd00be79bd813
Francesco <francesco.barabino@tiscali.it>
2020-05-20 12:54:25 +0200

    Data updating optimization

--- update_data.sh
+++ update_data.sh
@@ -1,5 +1,2 @@
-git clone https://github.com/pcm-dpc/COVID-19
+wget https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json
 
-mv COVID-19/dati-json/dpc-covid19-ita-regioni.json dpc-covid19-ita-regioni.json
-
-rm -rf COVID-19
