f7b6d028511e99a2eef2800201017f08fc6f4d7a
Francesco <francesco.barabino@tiscali.it>
2020-05-21 15:15:12 +0200

    Bug fixing, shows last date

--- update_data.sh
+++ update_data.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json
+wget https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json -O dpc-covid19-ita-regioni.json
 
